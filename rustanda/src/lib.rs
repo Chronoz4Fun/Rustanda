c01a5dd7517441d4fe0c12a8926aeac5be69f0bf
Chronoz <136992124+Chronoz4Fun@users.noreply.github.com>
2024-08-03 16:16:23 +0200

    just backup

--- rustanda/src/lib.rs
+++ rustanda/src/lib.rs
@@ -1,7 +1,8 @@
 // src/lib.rs
 
+use std::collections::HashMap;
 use std::fs::File;
-use std::io::{self, Read};
+use std::io::{self, Read, BufRead, BufReader};
 
 pub fn read_csv(file_path: &str) -> io::Result<String> {
     // Check if file is fine
@@ -22,7 +23,7 @@ pub fn read_csv(file_path: &str) -> io::Result<String> {
         // Read the file into the String
         file.read_to_string(&mut contents)?;
 
-        Ok(contents)
+        return Ok(contents);
     } else {
         return Err(io::Error::new(
             io::ErrorKind::NotFound,
@@ -34,6 +35,40 @@ pub fn read_csv(file_path: &str) -> io::Result<String> {
 fn file_exists_and_is_csv(file_path: &str) -> Result<bool, io::Error> {
     let metadata: std::fs::Metadata = std::fs::metadata(file_path)?;
     Ok(metadata.is_file() && file_path.ends_with(".csv"))
+}
+
+fn parse_csv_data(content: &File) -> Result<bool, io::Error>{
+
+    let delimiter = match determine_delimiter(content) {
+        Ok(detected_delimiter) =>  detected_delimiter,
+        Err(e) => { eprintln!("Failed to determine delimiter: {}", e); return Err(e);},
+    };
+
+    let mut reader: BufReader<&File> = BufReader::new(content);
+    let mut first_line: String = String::new();
+
+    return Ok(true);
+
+}
+
+fn determine_delimiter(content: &File) -> Result<char, io::Error>{
+    let mut reader: BufReader<&File> = BufReader::new(content);
+    let mut first_line: String = String::new();
+    reader.read_line(&mut first_line)?;
+
+    // remove extraneos whitespace
+    let first_line: &str = first_line.trim();
+    let possible_delimiter: [char; 4] = [',', ';', '\t', '|'];
+
+    let mut counts: HashMap<_, _> = std::collections::HashMap::new();
+    for delimiter in &possible_delimiter {
+        counts.insert(delimiter, first_line.matches(*delimiter).count());
+    }
+
+    // Find delimiter with maximum count
+    let (&detected_delimiter, _) = counts.iter().max_by_key(|&(_, count)| count).unwrap_or((&&',', &0));
+
+    return Ok(*detected_delimiter);
 }
 
 
@@ -88,5 +123,12 @@ mod test {
         let result: Result<bool, io::Error> = file_exists_and_is_csv(file_name);
         assert!(result.is_err());
         assert_eq!(result.err().unwrap().kind(), io::ErrorKind::NotFound);
+    }
+
+    #[test]
+    fn delimiter_determination_is_working() {
+        let content = read_csv(FILE_PATH_CSV);
+        let result = determine_delimiter(content.unwrap());
+        assert_eq!(result.unwrap(), ',');
     }
 }
